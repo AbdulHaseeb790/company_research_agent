11ad2074184647c788f051a77b3731155e438a5c
Haseeb <abdulhaseebjamali04@gmail.com>
2026-08-02 13:36:09 -0700

    Update api.py

--- api.py
+++ api.py
@@ -2,31 +2,30 @@ from fastapi import FastAPI
 from agent import research_company
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-app=FastAPI()
+
+app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://company-research-agent-1ecuqsa5q-wedev-015c.vercel.app",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
-
-
-)
 class ResearchRequest(BaseModel):
-    company_name:str
+    company_name: str
+
 class FollowupRequest(BaseModel):
-    question:str
-@app.post('/research')
-async def do_research(research:ResearchRequest):
+    question: str
+
+@app.post("/research")
+async def do_research(research: ResearchRequest):
     return await research_company(research.company_name)
-@app.post('/follow')
-async def do_follow(follow:FollowupRequest):
+
+@app.post("/follow")
+async def do_follow(follow: FollowupRequest):
     return await research_company(follow.question)
-    
-
-
-
